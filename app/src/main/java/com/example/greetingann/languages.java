156f8b3a8fac03a2add924f0cf09260533929776
Harkiran <hb9633@desales.edu>
2022-10-27 17:23:14 -0400

    hw complete

--- app/src/main/java/com/example/greetingann/languages.java
+++ app/src/main/java/com/example/greetingann/languages.java
@@ -8,8 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
-
+/**
+ * Angel Negron
+ *
+ */
 public class languages extends Fragment {
 
     @Override
@@ -17,6 +21,14 @@ public class languages extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_welcome, container, false);
+
+        //Tv text
+        TextView tvLang = view.findViewById(R.id.textView_Lang);
+
+        //Holds value of language passed from welcome screen
+        String lang = languagesArgs.fromBundle(requireArguments()).getTransLang();
+        tvLang.setText(lang);
+
 
 
         return view;
--- app/src/main/java/com/example/greetingann/welcomeScreen.java
+++ app/src/main/java/com/example/greetingann/welcomeScreen.java
@@ -3,12 +3,17 @@ package com.example.greetingann;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Spinner;
 
+/**
+ * Angel Negron
+ */
 public class welcomeScreen extends Fragment {
 
     @Override
@@ -16,9 +21,49 @@ public class welcomeScreen extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_languages, container, false);
-        //Button btnLang = view.findViewById(R.id.languages);
+        Button btnLang = view.findViewById(R.id.btn_translate);
+        Spinner spinOptions = view.findViewById(R.id.spinner);
+
+
+        //String transString = welcomeScreenArgs.fromBundle(requireArguments().getMessage());
+
+        btnLang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+
+                //Passed the string Language chosen by the user and translates it
+           welcomeScreenDirections.ActionWelcomeScreenToLanguages action
+                        = welcomeScreenDirections.actionWelcomeScreenToLanguages(chosenLang(spinOptions));
+
+           //Performs action by taking it to the proper screen
+                Navigation.findNavController(view).navigate(action);
+
+
+            }
+        });
+
+
 
 
         return view;
+    }
+
+    public String chosenLang(Spinner spin)
+    {
+       String lang = spin.getSelectedItem().toString();
+
+       if (lang.equals("Spanish"))
+       {
+           return "Hola";
+       }
+       else if (lang.equals("French"))
+       {
+           return "bonjour";
+       }
+       else
+        return "Hello";
     }
 }
