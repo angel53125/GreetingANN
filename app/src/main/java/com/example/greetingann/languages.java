f1d6cad8cbd552935321237538c28c75e36bca28
Harkiran <hb9633@desales.edu>
2022-10-27 10:49:26 -0400

    Initial commit

---
+++ app/src/main/java/com/example/greetingann/languages.java
@@ -0,0 +1,24 @@
+package com.example.greetingann;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class languages extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_welcome, container, false);
+
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/greetingann/welcomeScreen.java
@@ -0,0 +1,24 @@
+package com.example.greetingann;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class welcomeScreen extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_languages, container, false);
+        //Button btnLang = view.findViewById(R.id.languages);
+
+
+        return view;
+    }
+}
